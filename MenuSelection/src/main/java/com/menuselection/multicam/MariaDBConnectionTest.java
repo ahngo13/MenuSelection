b40631524ede0237835871a76430e1a7a08b00c7
Hamlet <ahngo13@naver.com>
2019-12-23 20:06:01 +0900

    test

--- MenuSelection/src/main/java/com/menuselection/multicam/MariaDBConnectionTest.java
+++ MenuSelection/src/main/java/com/menuselection/multicam/MariaDBConnectionTest.java
@@ -16,6 +16,7 @@ public class MariaDBConnectionTest {
         Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
         // Class.forName("com.mysql.jdbc.Driver"); MySQL
 
+        
         Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/test", "root", "1234");// 마리아DB
         // Connection con =     DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","passwd"); MySQL
         System.out.println(con);
