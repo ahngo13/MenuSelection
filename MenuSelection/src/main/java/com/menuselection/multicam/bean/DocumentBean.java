6aae8223dbf894430bf6374939fca1b70fe1ba74
Hamlet <ahngo13@naver.com>
2019-12-30 21:09:44 +0900

    KakaoBean update, DocumentBean add

---
+++ MenuSelection/src/main/java/com/menuselection/multicam/bean/DocumentBean.java
@@ -0,0 +1,90 @@
+package com.menuselection.multicam.bean;
+
+public class DocumentBean {
+	String placeName;
+    String distance;
+	String placeUrl;
+    String categoryName;
+	String addressName;
+	String roadAddressName;
+	String id;
+	String phone;
+	String categoryGroupCode;
+	String categoryGroupName;
+	String x;
+	String y;
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getPlaceUrl() {
+		return placeUrl;
+	}
+	public void setPlaceUrl(String placeUrl) {
+		this.placeUrl = placeUrl;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getAddressName() {
+		return addressName;
+	}
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
+	}
+	public String getRoadAddressName() {
+		return roadAddressName;
+	}
+	public void setRoadAddressName(String roadAddressName) {
+		this.roadAddressName = roadAddressName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCategoryGroupCode() {
+		return categoryGroupCode;
+	}
+	public void setCategoryGroupCode(String categoryGroupCode) {
+		this.categoryGroupCode = categoryGroupCode;
+	}
+	public String getCategoryGroupName() {
+		return categoryGroupName;
+	}
+	public void setCategoryGroupName(String categoryGroupName) {
+		this.categoryGroupName = categoryGroupName;
+	}
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	
+}
--- MenuSelection/src/main/java/com/menuselection/multicam/bean/KakaoBean.java
+++ MenuSelection/src/main/java/com/menuselection/multicam/bean/KakaoBean.java
@@ -1,26 +1,77 @@
 package com.menuselection.multicam.bean;
 
+import java.util.ArrayList;
+
 public class KakaoBean {
-	String keywordNm;
-	String currentPage;
-	String pageSize;
-	public String getKeywordNm() {
-		return keywordNm;
-	}
-	public void setKeywordNm(String keywordNm) {
-		this.keywordNm = keywordNm;
-	}
-	public String getCurrentPage() {
-		return currentPage;
-	}
-	public void setCurrentPage(String currentPage) {
-		this.currentPage = currentPage;
-	}
-	public String getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(String pageSize) {
-		this.pageSize = pageSize;
-	}
 	
+	//same_name
+	String region;
+	String keyword;
+    String selectedRegion;
+	
+	//meta
+	int pageableCount;
+	int totalCount;
+	boolean isEnd;
+	
+
+	ArrayList<DocumentBean> documentList;
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getSelectedRegion() {
+		return selectedRegion;
+	}
+
+	public void setSelectedRegion(String selectedRegion) {
+		this.selectedRegion = selectedRegion;
+	}
+
+	public int getPageableCount() {
+		return pageableCount;
+	}
+
+	public void setPageableCount(int pageableCount) {
+		this.pageableCount = pageableCount;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public boolean isEnd() {
+		return isEnd;
+	}
+
+	public void setEnd(boolean isEnd) {
+		this.isEnd = isEnd;
+	}
+
+	public ArrayList<DocumentBean> getDocumentList() {
+		return documentList;
+	}
+
+	public void setDocumentList(ArrayList<DocumentBean> documentList) {
+		this.documentList = documentList;
+	}
+    
+		
 }
