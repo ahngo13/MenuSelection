75fc2d000059606f9e4118431de0484321f765e8
pickle-radish <tkdansg0222@gmail.com>
2020-01-01 20:39:04 +0900

    create naver page

--- MenuSelection/src/main/java/com/menuselection/multicam/NaverController.java
+++ MenuSelection/src/main/java/com/menuselection/multicam/NaverController.java
@@ -1,5 +1,19 @@
 package com.menuselection.multicam;
 
-public class NaverController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class NaverController {
+	
+	@RequestMapping(value = "/naver", method = RequestMethod.GET)
+	public String kakao(Model model, @RequestParam String value) throws Exception {
+
+		model.addAttribute("value", value);
+		return "naver";
+		
+	}
 }
